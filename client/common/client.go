0f210e3d531a369c02ba1f08fa701710a0adc2ab
valenpontaut <valenpontaut@gmail.com>
2026-03-24 21:22:50 -0300

    feat: update client to send bet and receive confirmation

--- client/common/client.go
+++ client/common/client.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"bufio"
-	"fmt"
 	"net"
 	"os"
 	"os/signal"
@@ -48,6 +46,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
@@ -58,50 +57,33 @@ func (c *Client) StartClientLoop() {
 	sigterm := make(chan os.Signal, 1)
 	signal.Notify(sigterm, syscall.SIGTERM)
 
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		select {
-		case <-sigterm:
-			log.Infof("action: shutdown | result: success | client_id: %v", c.config.ID)
-			return
-		default:
-		}
-
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-		defer c.conn.Close()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
-		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		select {
-		case <-sigterm:
-			log.Infof("action: shutdown | result: success | client_id: %v", c.config.ID)
-			return
-		case <-time.After(c.config.LoopPeriod):
-		}
+	bet, err := NewBetFromEnv()
+	if err != nil {
+		log.Criticalf("action: apuesta_leida | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+
+	select {
+	case <-sigterm:
+		log.Infof("action: shutdown | result: success | client_id: %v", c.config.ID)
+		return
+	default:
+	}
+
+	if err := c.createClientSocket(); err != nil {
+		return
+	}
+	defer c.conn.Close()
+
+	if err := SendBet(c.conn, c.config.ID, bet); err != nil {
+		log.Errorf("action: apuesta_enviada | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
+	}
+
+	if _, err := RecvConfirmation(c.conn); err != nil {
+		log.Errorf("action: recibir_confirmacion | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
+	}
+
+	log.Infof("action: apuesta_enviada | result: success | dni: %v | numero: %v", bet.dni, bet.numero)
 }
